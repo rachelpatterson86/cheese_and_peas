8d14efb2e46adc0bf7de611a712fd6a63cebdc0c
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-20 00:28:21 -0400

    add serialization and validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,11 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_many :excluded_ingredients
+  validates :user, uniqueness: true
+  has_many :excluded_ingredients 
   has_many :allowed_allergies
   has_many :allowed_diets
+
+  serialize :excluded_ingredients
+  serialize :allowed_allergies
+  serialize :allowed_diets
 end
