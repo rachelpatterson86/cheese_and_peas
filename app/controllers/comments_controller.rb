2faadc944b30f3bef3879b20dd3b955e7c667328
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-24 10:48:22 -0400

    comments form works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
-    @comment = @recipe.comments.create(:comment => params[:comment][:body])
-    respond_to do |format|
-      format.html { redirect_to [@user, @recipe] }
-      format.js { render :create }
-    end
-  end
-
-  def create
   # Recipe yummly Id should get passed as hidden input in both cases.
-    @yummly = Yummly.find(params[:rid])
+    @yummly = Yummly.find(params[:comment][:rid])
     @recipe = Recipe.where(yummly_id: @yummly.id).
       first_or_create(description: @yummly.name)
-    redirect_to recipe_path(@recipe)
+    #redirect_to recipe_path(@recipe)
+    @comment = @recipe.comments.create(:comment => params[:comment][:comment])
+    respond_to do |format|
+      format.html { redirect_to [@recipe] } #recipe_path(id: @recipe.yummly_id)
+      format.js { render :create }
+    end
   end
 
 
