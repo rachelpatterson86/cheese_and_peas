ddf5983f1a7539b239020666a879e2e5628bf982
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-24 16:03:45 -0400

    search and headers look good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,20 @@
 class HomeController < ApplicationController
 
   def search
-
-    if current_user.try(:profile)
-      @profile = current_user.profile
-      @query = Yummly.search(params[:q],
-               params={ :requirePictures => true,
-                        :excludedIngredient => @profile.excluded_ingredients,
-                        :allowedAllergy => @profile.allowed_allergies,
-                        :allowedDiet => @profile.allowed_diets })
-    else
-      @query = Yummly.search(params[:q], params={:requirePictures => true})
-    #@query = Yummly.search(params[:q], params={:requirePictures => true})
-    #undefined method `[]' for nil:NilClass
-  # end
+    unless params[:q].nil?
+      if current_user.try(:profile)
+        @profile = current_user.profile
+        @query = Yummly.search(params[:q],
+                 params={ :requirePictures => true,
+                          :excludedIngredient => @profile.excluded_ingredients,
+                          :allowedAllergy => @profile.allowed_allergies,
+                          :allowedDiet => @profile.allowed_diets })
+      else
+        @query = Yummly.search(params[:q], params={:requirePictures => true})
+      #@query = Yummly.search(params[:q], params={:requirePictures => true})
+      #undefined method `[]' for nil:NilClass
+    # end
+      end
     end
   end
 
