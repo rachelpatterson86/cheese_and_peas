ec96c2e35dd14acf38a7678ea8af7579487eb8aa
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-24 23:25:01 -0400

    search results done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,16 @@ class HomeController < ApplicationController
         @profile = current_user.profile
         @query = Yummly.search(params[:q],
                  params={ :requirePictures => true,
+                          :maxResult=>12,
+                          :start=>12,
                           :excludedIngredient => @profile.excluded_ingredients,
                           :allowedAllergy => @profile.allowed_allergies,
                           :allowedDiet => @profile.allowed_diets })
       else
-        @query = Yummly.search(params[:q], params={:requirePictures => true})
-      #@query = Yummly.search(params[:q], params={:requirePictures => true})
-      #undefined method `[]' for nil:NilClass
-    # end
+        @query = Yummly.search(params[:q], params={:requirePictures => true,
+                                                   :maxResult=>12,
+                                                   :start=>12})
+
       end
     end
   end
